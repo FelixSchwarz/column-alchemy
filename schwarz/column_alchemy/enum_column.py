50adad34c630897f4ca291f1b5561b8117a7a311
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2019-09-02 10:43:57 +0200

    ValuesEnum: adapt to SQLAlchemy >= 1.2.3

--- schwarz/column_alchemy/enum_column.py
+++ schwarz/column_alchemy/enum_column.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017 Felix Schwarz
+# Copyright (c) 2017, 2019 Felix Schwarz
 # The source code contained in this file is licensed under the MIT license.
 # SPDX-License-Identifier: MIT
 
 from sqlalchemy.types import Enum as SQLEnum
 
 
-__all__ = ['ValuesEnum']
+__all__ = [
+    'get_enum_value',
+    'ValuesEnum',
+]
 
-class ValuesEnum(SQLEnum):
+def ValuesEnum(enum_class, **enum_kwargs):
     """This is pretty similar to SQLAlchemy's Enum type.
     However SQLAlchemy stores the NAMES of enum options in the databases
     instead of their associated values.
@@ -23,24 +26,30 @@ class ValuesEnum(SQLEnum):
       arbitrary Python types without bijective serialization.
     - Enum values are not necessarily unique (unless @unique is used)
 
-    However I found SQLAlchemy's behaviour highly confusing as I tend use
-    enum names similar to variable names to provide better code readability.
-    This class will store the enum VALUES in the database.
+    However I found SQLAlchemy's default behaviour highly confusing as I tend
+    use enum names similar to variable names to provide better code readability.
+    Often I like to to store the enum VALUES in the database.
 
-    Currently this is really a bare-bones implementation so it comes with two
+    SQLAlchemy >= 1.2.3 introduced the "values_callable" parameter so there is
+    no need for a custom column implementation. However this module provides
+    some nice helpers so there is no code duplication across different projects.
+
+    The current implementation is pretty basic so it comes with two
     important limitations:
     1. Enum values must be unique. No effort is made to ensure that this is
        really the case.
     2. Enum values must be strings.
     """
-    # override SQLAlchemy's Enum._parse_into_values()
-    def _parse_into_values(self, enums, kw):
-        if len(enums) == 1 and hasattr(enums[0], '__members__'):
-            self.enum_class = enums[0]
-            objects = tuple(self.enum_class.__members__.values())
-            values = [o.value for o in objects]
-            kw.setdefault('name', self.enum_class.__name__.lower())
-            return values, objects
-        else:
-            self.enum_class = None
-            return enums, enums
+    return SQLEnum(enum_class, values_callable=get_enum_values, **enum_kwargs)
+
+
+def get_enum_value(enum_obj):
+    # Ad-hoc instances (not loaded from SQLAlchemy) do not have a real
+    # enum instance. In these cases we get a plain string which represents the
+    # wanted value.
+    value = enum_obj.value if hasattr(enum_obj, 'value') else enum_obj
+    return value
+
+def get_enum_values(enum_class):
+    return [item.value for item in enum_class]
+
