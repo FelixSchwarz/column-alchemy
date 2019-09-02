917401ac640174ef52868d443b1e2e0fece41fbc
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2019-09-02 11:04:54 +0200

    extract EnumDBTestCase so it can be reused

--- schwarz/column_alchemy/tests/enum_column_test.py
+++ schwarz/column_alchemy/tests/enum_column_test.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017 Felix Schwarz
+# Copyright (c) 2017, 2019 Felix Schwarz
 # The source code contained in this file is licensed under the MIT license.
 # SPDX-License-Identifier: MIT
 
@@ -7,38 +7,21 @@ from enum import Enum
 
 from ddt import ddt as DataDrivenTestCase, data
 from pythonic_testcase import *
+import sqlalchemy
 from sqlalchemy import create_engine, Column, MetaData, Table
 from sqlalchemy.orm import sessionmaker
 
 from ..enum_column import ValuesEnum
 
 
-def create_session(engine):
-    Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    return Session()
 
-
-@DataDrivenTestCase
-class ValuesEnumTest(PythonicTestCase):
+class EnumDBTestCase(PythonicTestCase):
     def setUp(self):
-        super(ValuesEnumTest, self).setUp()
+        super(EnumDBTestCase, self).setUp()
         self.engine = create_engine('sqlite:///:memory:')
         self.connection = self.engine.connect()
 
-    @data('eins', 'zwei', None)
-    def test_can_store_and_load_values(self, value):
-        class FooEnum(Enum):
-            one = 'eins'
-            two = 'zwei'
-        value2enum = dict((e.value, e) for e in FooEnum.__members__.values())
-
-        value_column = Column('value', ValuesEnum(FooEnum))
-        table = self._init_table_with_values([value_column], [{'value': value}])
-        session = create_session(self.engine)
-        db_value = session.query(table).limit(1).scalar()
-        expected_enum = value2enum.get(value)
-        assert_equals(expected_enum, db_value)
-
+    # --- internal helpers ----------------------------------------------------
     def _init_table_with_values(self, columns, insertions=None):
         metadata = MetaData(bind=self.connection)
         table = Table('foo', metadata, *columns)
@@ -49,3 +32,30 @@ class ValuesEnumTest(PythonicTestCase):
 
     def _insert_data(self, table, insertions):
         self.connection.execute(table.insert(), insertions)
+
+    def _fetch_value(self, table):
+        "Fetches the DB values via SQLAlchemy (so we should get Enum values)."
+        session = self._create_session(self.engine)
+        db_value = session.query(table).limit(1).scalar()
+        return db_value
+
+    def _create_session(self, engine):
+        Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+        return Session()
+
+
+
+@DataDrivenTestCase
+class ValuesEnumTest(EnumDBTestCase):
+    @data('eins', 'zwei', None)
+    def test_can_store_and_load_values(self, value):
+        class FooEnum(Enum):
+            one = 'eins'
+            two = 'zwei'
+        value2enum = dict((e.value, e) for e in FooEnum.__members__.values())
+
+        value_column = Column('value', ValuesEnum(FooEnum))
+        table = self._init_table_with_values([value_column], [{'value': value}])
+        expected_enum = value2enum.get(value)
+        assert_equals(expected_enum, self._fetch_value(table))
+
